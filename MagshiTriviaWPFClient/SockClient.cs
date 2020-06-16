a082d3694adf5069f1cff2be336ebe71674f0c54
Tom Chengich <tomcengich@gmail.com>
2020-06-16 20:50:24 +0300

    Basic SockClient implemented.

---
+++ MagshiTriviaWPFClient/SockClient.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Threading;
+
+namespace MagshiTriviaWPFClient
+{
+
+    public class StateObject 
+    {  
+        public Socket clientSock = null;  
+        public byte[] buffer = null;  
+    }
+    class SockClient
+    {
+        private Socket clientSock;
+        private ManualResetEvent receiveDone; 
+        public SockClient()
+        {
+            try
+            {
+                this.clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                this.clientSock.BeginConnect(new IPEndPoint(IPAddress.Loopback, 5000), new AsyncCallback(ConnectCallBack), null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void ConnectCallBack(IAsyncResult AR)
+        {
+            try
+            {
+                this.clientSock.EndConnect(AR);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void Send(byte[] message)
+        {
+            try
+            {
+                this.clientSock.BeginSend(message, 0, message.Length, SocketFlags.None, new AsyncCallback(SendCallback), null);
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Connection To Server Lost.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void SendCallback(IAsyncResult AR)
+        {
+            try
+            {
+                this.clientSock.EndSend(AR);
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Connection To Server Lost.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private byte[] Receive()
+        {
+            receiveDone = new ManualResetEvent(false);
+            StateObject state = new StateObject();
+            state.clientSock = this.clientSock;
+            state.buffer = new byte[this.clientSock.ReceiveBufferSize];
+            this.clientSock.BeginReceive(state.buffer, 0, state.buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), state);
+            receiveDone.WaitOne();
+            return state.buffer;
+        }
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            try
+            {
+                this.clientSock.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Connection To Server Lost.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            receiveDone.Set();
+        }
+    }
+}

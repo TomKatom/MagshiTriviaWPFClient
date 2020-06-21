af636f76cedf4e2747c749701f68bcde42921445
Tom Chengich <tomcengich@gmail.com>
2020-06-21 21:36:02 +0300

    Register and Login Response and Request Classes ready.

---
+++ MagshiTriviaWPFClient/SerializationAndDeserialization/RequestClasses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagshiTriviaWPFClient
+{
+    public class RegisterRequest
+    {
+        public RegisterRequest(string username, string email, string password)
+        {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+        }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+    public class LoginRequest
+    {
+        public LoginRequest(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ MagshiTriviaWPFClient/SerializationAndDeserialization/ResponseClasses.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagshiTriviaWPFClient
+{
+
+    enum ResponseCodes
+    {
+        LoginSuccess = 0,
+        LoignError
+    }
+
+    public class LoginResponse
+    {
+        public LoginResponse(ushort status)
+        {
+            this.status = status;
+        }
+        public ushort status { get; set; }
+    }
+}

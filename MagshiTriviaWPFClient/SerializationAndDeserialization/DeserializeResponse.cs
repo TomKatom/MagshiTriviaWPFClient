ac0a3adbe2a473267ef866120e8204e65fe1e2c1
Tom Chengich <tomcengich@gmail.com>
2020-06-21 21:35:24 +0300

    Basic Login and Register Serialization and Deserialization code ready.

---
+++ MagshiTriviaWPFClient/SerializationAndDeserialization/DeserializeResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MagshiTriviaWPFClient.SerializationAndDeserialization
+{
+    public static class DeserializeResponse
+    {
+        public static LoginResponse DeserializeLogin(byte[] data)
+        {
+           
+            int dataLength = ((data[1] << 8 | data[2]) << 8 | data[3]) << 8 | data[4];
+            string jsonData = Encoding.ASCII.GetString(data, 5, dataLength);
+            LoginResponse result = JsonConvert.DeserializeObject<LoginResponse>(jsonData);
+            return result;
+        }
+    }
+}
---
+++ MagshiTriviaWPFClient/SerializationAndDeserialization/SerializeRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MagshiTriviaWPFClient
+{
+    static class SerializeRequest
+    {
+        public static byte[] SerializeRegister(RegisterRequest request)
+        {
+            string data = JsonConvert.SerializeObject(request);
+            ushort code = 21;
+            uint dataLength = (uint)data.Length;
+            byte[] serialized = new byte[1 + 4 + dataLength];
+            serialized[0] = BitConverter.GetBytes(code)[0];
+            for(int i = 1; i < 5; i++)
+            {
+                serialized[i] = BitConverter.GetBytes(dataLength)[i-1];
+            }
+            for(int i = 5, j = 0; i < serialized.Length; i++, j++)
+            {
+                serialized[i] = ASCIIEncoding.ASCII.GetBytes(data)[j];
+            }
+            return serialized;
+        }
+        public static byte[] SerializeLogin(LoginRequest request)
+        {
+            string data = JsonConvert.SerializeObject(request);
+            ushort code = 20;
+            uint dataLength = (uint)data.Length;
+            byte[] serialized = new byte[1 + 4 + dataLength];
+            serialized[0] = BitConverter.GetBytes(code)[0];
+            for(int i = 1; i < 5; i++)
+            {
+                serialized[i] = BitConverter.GetBytes(dataLength)[i-1];
+            }
+            for(int i = 5, j = 0; i < serialized.Length; i++, j++)
+            {
+                serialized[i] = ASCIIEncoding.ASCII.GetBytes(data)[j];
+            }
+            return serialized;
+        }
+    }
+}
